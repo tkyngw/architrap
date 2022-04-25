85ece611e0c08ea7a0b4bf5ea0005b7bfa3c6003
matigobbi <matigobbi@gmail.com>
2022-04-25 17:41:38 +0200

    merge

--- models/User.js
+++ models/User.js
@@ -8,11 +8,11 @@ const userSchema = new Schema(
       required: [true, 'Username is required.'],
       unique : true,
     },
-    password:{ 
+    password: {
       type: String,
-    required: [true, 'Password is required.']
-   }
-  },
+      required: [true, 'Password is required.']
+    },
+  }
 );
 
 const User = model("User", userSchema);
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,3 +1,6 @@
+const { compile } = require("hbs");
+const Company = require("../models/Company");
+
 const router = require("express").Router();
 
 /* GET home page */
@@ -7,7 +10,22 @@ router.get("/", (req, res, next) => {
   res.render("index" , { user: loggedInUser });
 });
 
-
+router.get('/result', (req, res, next) => {
+  const queryString = req.query.q
+  console.log(queryString)
+  Company.findOne({name : queryString})
+  .then(comapniesFromDB => {
+    if(comapniesFromDB === null){
+      res.render('result', { message : 'Sorry, no results found'})
+      return
+    } else if (comapniesFromDB.name === queryString){
+    res.render('result', { companies : comapniesFromDB})
+    } 
+  })
+  .catch(err => {
+    next(err)
+  })
+})
 
 module.exports = router;
 
