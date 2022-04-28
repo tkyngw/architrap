98d13920aeabc657dec2c92fff0b5d47ef78e7e4
Takyoung Woo <takyoung.w@gmail.com>
2022-04-28 14:24:28 +0200

    minor style

--- models/Review.js
+++ models/Review.js
@@ -11,6 +11,10 @@ const reviewSchema = new Schema({
     diversity: String,
     benefits: String,
     comments : String,
+    owner: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
 });
 
 const Review = mongoose.model('Review', reviewSchema);
