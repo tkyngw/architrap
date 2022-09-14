be42e97afe6439740cfc079d6086a78051c04b4a
matigobbi <matigobbi@gmail.com>
2022-09-14 09:41:29 +0200

    make it better

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,7 +7,6 @@ const router = require("express").Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  const loggedInUser = req.user
   // console.log(req.user)
   res.render("index" , { user: req.user});
 });
